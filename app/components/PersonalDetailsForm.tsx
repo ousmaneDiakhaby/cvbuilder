a5cf1a3e4bf4223a77abb58a0313da2d205a79ce
ousmaneDiakhaby <diakhaby.ousmane@outlook.com>
2024-12-19 19:22:02 +0100

    initial commit from create-next-app

---
+++ app/components/PersonalDetailsForm.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {PersonalDetails} from "@/type";
+
+type Props = {
+  personalDetails: PersonalDetails;
+  setPersonalDetails: (pd: PersonalDetails) => void;
+  setFile: (file: File | null) => void;
+};
+
+const PersonalDetailsForm: React.FC<Props> = ({personalDetails, setPersonalDetails, setFile}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, field: keyof PersonalDetails) => {
+    setPersonalDetails({...personalDetails, [field]: e.target.value});
+  }
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>, field: keyof PersonalDetails) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <input
+        type="text"
+        placeholder="Nom complet"
+        value={personalDetails.fullName}
+        onChange={(e) => handleChange(e, 'fullName')}
+        className="input input-bordered w-full"
+      />
+      <div className="flex">
+        <input
+          type="email"
+          placeholder="Email"
+          value={personalDetails.email}
+          onChange={(e) => handleChange(e, 'email')}
+          className="input input-bordered w-full"
+        />
+        <input
+          type="tel"
+          placeholder="Numéro de téléphone"
+          value={personalDetails.phone}
+          onChange={(e) => handleChange(e, 'phone')}
+          className="input input-bordered w-full ml-4"
+        />
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Adresse"
+          value={personalDetails.address}
+          onChange={(e) => handleChange(e, 'address')}
+          className="input input-bordered w-full"
+        />
+
+        <input
+          type="file"
+          accept="image/*"
+          placeholder="Photo"
+          onChange={(e) => handleFileChange(e, 'photoUrl')}
+          className="file-input file-input-bordered w-full file-input-primary"
+        />
+
+        <input
+        type="text"
+        placeholder="Poste recherché"
+        value={personalDetails.description}
+        onChange={(e) => handleChange(e, 'postSeeking')}
+        className="input input-bordered w-full"
+        />
+
+        <textarea
+          placeholder="Description"
+          value={personalDetails.description}
+          onChange={(e) => handleChange(e, 'description')}
+          className="textarea textarea-bordered w-full"
+        />
+
+      </div>
+
+    </div>
+  );
+};
+
+export default PersonalDetailsForm;
